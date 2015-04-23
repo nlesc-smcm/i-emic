5cbfefd3ce67906a603e92790b7824f677ccb2d0
erik <erik@imac_imau>
2015-04-23 14:21:45 +0200

    ThetaStepper is maturing. Test does a timestepping run of 50 yrs from motionless solution and seems to work properly.

--- test/test.C
+++ test/test.C
@@ -1,7 +1,11 @@
+//==============================================================================
+// Test routine
+//==============================================================================
 // This defines useful macros like HAVE_MPI, which is defined if and
 // only if Epetra was built with MPI enabled.
 #include <Epetra_config.h>
 
+//==============================================================================
 #  ifdef HAVE_MPI
 // Epetra's wrapper for MPI_Comm.  This header file only exists if
 // Epetra was built with MPI enabled.
@@ -11,37 +15,41 @@
 #    include <Epetra_SerialComm.h>
 #  endif // HAVE_MPI
 
+//==============================================================================
 #include <Teuchos_RCP.hpp>
 #include <Teuchos_FancyOStream.hpp>
-#include <Teuchos_XMLParameterListHelpers.hpp>
 
 #include <Epetra_Vector.h>
 
 #include "GlobalDefinitions.H"
 #include "Ocean.H"
 #include "ThetaStepper.H"
-#include "Singleton.H"
 
+//==============================================================================
 using Teuchos::RCP;
 using Teuchos::rcp;
 
+//==============================================================================
+// A few declarations
 RCP<std::ostream> outFile;
 RCP<std::ostream> outputFiles(RCP<Epetra_Comm> Comm);
+RCP<Epetra_Comm>  initializeEnvironment(int argc, char **argv);
 
+//==============================================================================
 int main(int argc, char **argv)
 {
-#ifdef HAVE_MPI
-	MPI_Init(&argc, &argv);
-	RCP<Epetra_MpiComm> Comm =
-		rcp(new Epetra_MpiComm(MPI_COMM_WORLD));
-#else
-	RCP<Epetra_SerialComm> Comm =
-		rcp(new Epetra_SerialComm());
-#endif
-	// Specify output streams
-	outFile = outputFiles(Comm);
-	// Initialize ocean model (THCM)
+	// Initialize the environment:
+	//  - MPI
+	//  - output files
+	//  - returns Trilinos' communicator Epetra_Comm
+	RCP<Epetra_Comm> Comm = initializeEnvironment(argc, argv);
+	
+ 	// Create timestepping (theta method) ocean model OceanTheta
+	//  (based on THCM):
 	RCP<OceanTheta> ocean = rcp(new OceanTheta(Comm));
+
+	// Create timestepping object ThetaStepper using an OceanTheta model
+	//  and an Epetra_Vector
 	ThetaStepper<RCP<OceanTheta>, RCP<Epetra_Vector> >
 		thetaStepper(ocean, ocean->GetState());
 	
@@ -54,6 +62,24 @@ int main(int argc, char **argv)
 	MPI_Finalize();	
 }
 
+//==============================================================================
+RCP<Epetra_Comm> initializeEnvironment(int argc, char **argv)
+{
+	
+#ifdef HAVE_MPI
+	MPI_Init(&argc, &argv);
+	RCP<Epetra_MpiComm> Comm =
+		rcp(new Epetra_MpiComm(MPI_COMM_WORLD));
+#else
+	RCP<Epetra_SerialComm> Comm =
+		rcp(new Epetra_SerialComm());
+#endif
+	// Specify output files
+	outFile = outputFiles(Comm);
+	return Comm;
+}
+
+//==============================================================================
 Teuchos::RCP<std::ostream> outputFiles(Teuchos::RCP<Epetra_Comm> Comm)
 {
 	// Setup output files "fname_#.txt" for P==0 && P==1, other processes
