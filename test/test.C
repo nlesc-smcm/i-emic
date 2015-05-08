269cb76491faae07770d0044463a0b3b4d3deaec
erik <erik@groningen>
2015-05-08 10:26:40 +0200

    -Improved dependencies in ocean makefile -Implementation of Vector should have a source file Vector.C -Created Euler predictor in continuation class

--- test/test.C
+++ test/test.C
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
 	Continuation<RCP<OceanCont>, RCP<Vector> >
 		continuation(ocean);
 
-	continuation.Run();
+	continuation.Step();
 	
 	ocean->DumpState();
 	
---
+++ vector/Vector.C
@@ -0,0 +1,4 @@
+#include "Vector.H"
+
+
+// PUT THE IMPLEMENTATION HERE
