ad0f14ff132b81dc43c0998bb32f26874ba2a7ab
erik <erik@groningen>
2015-06-11 10:33:30 +0200

    Added comments

--- ocean/atm.F90
+++ ocean/atm.F90
@@ -2,13 +2,14 @@
 module m_atm
 
 
-      real, parameter ::  hdima = 8400.
-      real, parameter ::   rhoa = 1.25
-      real, parameter ::   uatm = 0.0
-      real, parameter ::     ce = 1.3e-03
-      real, parameter ::     ch = 0.94 * ce  ! exchange coefficient 
-      real, parameter ::    cpa = 1000.
-      real, parameter ::     uw = 8.5        ! -> mu=2.5
+      real, parameter ::  hdima = 8400.      ! atmospheric scale height \[H_a\]
+      real, parameter ::   rhoa = 1.25       ! atmospheric density \[\rho_a\]
+      real, parameter ::   uatm = 0.0        ! advection?? not in the paper
+      real, parameter ::     ce = 1.3e-03    ! exchange coefficient 
+      real, parameter ::     ch = 0.94 * ce  ! exchange coefficient \[C_H\]
+      real, parameter ::    cpa = 1000.      ! heat capacity \[C_{pa}\]
+      real, parameter ::     uw = 8.5        ! mean atmospheric surface wind speed
+                                             ! \[|V_a|\] --> mu \approx 13
       real, parameter ::     d0 = 3.1e+06    ! constant eddy diffusivity \[D_0\]
       real, parameter ::     c0 = 0.43       ! atmospheric absorption coefficient
       real, parameter :: sigmab = 5.67e-08   ! 
