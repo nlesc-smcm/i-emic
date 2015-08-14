d9e11f75e00ad41294526352bd4d1bb85d76d5e4
erik <erik@groningen>
2015-08-14 18:13:11 +0200

    Added visualization of profile scripts:   plot_profile in matlab and an accompanying bash script converting   the profile to a matlab readable file

---
+++ matlab/plot_profile.m
@@ -0,0 +1,20 @@
+function [profile] = plot_profile(filename, range)
+  % we assume that you are in a directory below the main
+  % project directory, for instance proj_dir/rundir
+  system(['../scripts/gatherprofile.sh ' filename ' converted']);
+  profile = importdata('converted');
+  N       = profile.data(1); % number of experiments
+  M       = numel(profile.textdata);
+  cores   = 2.^(0:N-1);
+  x_axis  = 1:N;
+  if nargin == 1
+	range = 1:M;
+  end
+  for i = range
+	semilogy(x_axis, profile.data((i-1)*N+2:i*N+1),'.-');
+	hold on
+  end
+  hold off
+  legend(profile.textdata(range));
+	  
+end
--- scripts/gatherprofile.sh
+++ scripts/gatherprofile.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
+# This script transforms the profile output to a format that matlab can read
+# Author: Erik -> t.e.mulder@uu.nl
 
-iname=profile_16x16
-oname=gatheredprofile
+# Specify in and output files
+if [ $# -ne 2 ]
+then
+	iname=profile_16x16
+	oname=gatheredprofile
+else
+	iname=$1
+	oname=$2
+fi
 
-echo > $oname
+delim=" " # Delimiting character
 
+echo -n > $oname # Initialize output file
+
+# Write the different program component names to the output
 for i in {1..20}
 do
 	grep -m 1 "($i)" $iname | sed 's/.*)\ *//' | sed 's/\ * :.*//' >> $oname
-	echo >> $oname
+done
+
+# Write the number of experiments to the output
+echo $delim >> $oname
+grep -c "(1)" $iname >> $oname
+echo $delim >> $oname
+
+# Write the total time results to the output
+for i in {1..20}
+do
 	grep "($i)" $iname | sed 's/.*:\ *//' | sed 's/\ .*//' >> $oname
-	echo >> $oname
+	echo $delim >> $oname
 done
