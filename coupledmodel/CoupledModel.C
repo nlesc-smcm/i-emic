7adc6a436a8370a4854a9131e93f432c1342ed83
erik <erik@imac_imau>
2015-09-15 11:43:19 +0200

    Added printProfile to coupledmodel -> Now we have 2 of these functions, we need something to manage profiles properly. Reduced standard problem size Added -Wall

--- coupledmodel/CoupledModel.C
+++ coupledmodel/CoupledModel.C
@@ -395,6 +395,68 @@ void CoupledModel::postProcess()
 	// Let the models do their own post-processing
 	ocean_->postProcess();
 	atmos_->postProcess();
+
+	// Print the contents of the profile
+	printProfile(profile);
+}
+
+//------------------------------------------------------------------
+// This is a copy of the code in main.C --> should be factorized
+void CoupledModel::printProfile(ProfileType profile)	
+{
+	if (timerStack.empty() == false)
+		WARNING("Unequal amount of TIMER_START and TIMER_STOP uses",
+				__FILE__, __LINE__);
+	
+	std::ostringstream profilefile("profile_output");   // setting up a filename
+	std::ofstream file(profilefile.str().c_str());      // setup output file
+
+	// Set format flags
+	file << std::left;
+
+	// Define line format
+#ifndef LINE
+# define LINE(s1, s2, s3, s4, s5, s6, s7, s8, s9)						\
+	{																	\
+		int sp = 3;  int it = 5;  int id = 5;							\
+		int db = 12; int st = 45;										\
+		file << std::setw(id) << s1	<< std::setw(sp) << s2				\
+			 << std::setw(st) << s3 << std::setw(sp) << s4				\
+			 << std::setw(db) << s5	<< std::setw(sp) << s6				\
+			 << std::setw(it) << s7	<< std::setw(sp) << s8				\
+			 << std::setw(db) << s9	<< std::endl;						\
+	}
+#endif
+
+	// Header
+	LINE("", "", "", "", "cumul.", "", "calls", "", "average");
+	
+	// Display timings of the separate models, summing
+	int counter = 0;
+	for (auto const &map : profile)
+		if (map.first.compare(0,5,"(itr)") != 0)
+		{
+			counter++;
+			std::stringstream s;
+			s << " (" << counter << ")";
+			LINE(s.str(), "", map.first, ":", map.second[0], "",
+				 map.second[1], "", map.second[2]);
+		}
+
+	// Newline
+	file << std::endl;
+	
+	// Display iteration information
+	for (auto const &map : profile)
+		if (map.first.compare(0,5,"(itr)") == 0 )
+		{
+			counter++;
+			std::stringstream s;
+			s << " (" << counter << ")";
+			LINE(s.str(), "", map.first.substr(5), ":", map.second[0], "",
+				 map.second[1], "", map.second[2]);
+		}
+	
 }
 
 //------------------------------------------------------------------
