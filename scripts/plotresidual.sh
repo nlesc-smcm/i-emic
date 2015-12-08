957366eb655cf92d7a5d55eb5b476588d9240f3e
erik <cartesius>
2015-12-08 14:10:53 +0100

    Improved residual plotscript

--- scripts/plotresidual.sh
+++ scripts/plotresidual.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # You might have to change this
-solvehistory=50
-newtonhistory=30
+solvehistory=$1
+newtonhistory=$2
+horizontal=100
+vertical=30
+date=`date +%m%d%y-%k%M`
+
 less dump | grep 'impl res' | tail -n $solvehistory | sed 's/.*iteration://' | sed 's/impl res://' \
 	| sed 's/expl.*//' > solveresiduals
 less info_0.txt | grep '||R||' | tail -n $newtonhistory | sed 's/.*||R||://' > newtonresiduals
@@ -10,19 +14,20 @@ less info_0.txt | grep '||R||' | tail -n $newtonhistory | sed 's/.*||R||://' > n
 
 if [ -s "solveresiduals" ]
 then
-	gnuplot -e "set terminal dumb 80 20;\
+	gnuplot -e "set terminal dumb $horizontal $vertical;\
                 set logscale y;\
                 unset xtics;\
-                set xrange [0:$solvehistory];\
                 set title 'linear solver';\
-                plot 'solveresiduals' using 2 with lines linetype 0 notitle;"
+                plot 'solveresiduals' using 2 with lines linetype 1 notitle;" > solveresidual.plot
+	cat solveresidual.plot
 fi
 
 if [ -s "newtonresiduals" ]
 then
-	gnuplot -e "set terminal dumb 80 20;\
+	gnuplot -e "set terminal dumb $horizontal $vertical;\
                 set logscale y;\
                 unset xtics;\
                 set title 'Newton iterations';\
-                plot 'newtonresiduals' using 1 with lines linetype 0 notitle"		
+                plot 'newtonresiduals' using 1 with lines linetype 0 notitle" > newtonresidual.plot
+	cat newtonresidual.plot
 fi
