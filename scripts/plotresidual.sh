cd1116353cf610c97902115e119b8cf96f3ca787
erik <erik@groningen>
2015-12-08 13:37:06 +0100

    Improver realtime residual plotscript

--- scripts/plotresidual.sh
+++ scripts/plotresidual.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
 # You might have to change this
-less dump | grep 'impl res' | tail -n 200 | sed 's/.*iteration://' | sed 's/impl res://' \
-    | sed 's/expl.*//' > residuals
+solvehistory=50
+newtonhistory=30
+less dump | grep 'impl res' | tail -n $solvehistory | sed 's/.*iteration://' | sed 's/impl res://' \
+	| sed 's/expl.*//' > solveresiduals
+less info_0.txt | grep '||R||' | tail -n $newtonhistory | sed 's/.*||R||://' > newtonresiduals
 
-gnuplot -e "set terminal dumb; plot 'residuals' using 1:2"
+
+if [ -s "solveresiduals" ]
+then
+	gnuplot -e "set terminal dumb 80 20;\
+                set logscale y;\
+                unset xtics;\
+                set xrange [0:$solvehistory];\
+                set title 'linear solver';\
+                plot 'solveresiduals' using 2 with lines linetype 0 notitle;"
+fi
+
+if [ -s "newtonresiduals" ]
+then
+	gnuplot -e "set terminal dumb 80 20;\
+                set logscale y;\
+                unset xtics;\
+                set title 'Newton iterations';\
+                plot 'newtonresiduals' using 1 with lines linetype 0 notitle"		
+fi
