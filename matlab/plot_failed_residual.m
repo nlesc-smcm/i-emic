941afb38217aba4955d487eb1dc44e2b996877be
erik <erik@groningen>
2016-03-10 10:29:38 +0100

    Printing every Levitus miss
    Changed problem setup
    Improved plot_failed_residual script

--- matlab/plot_failed_residual.m
+++ matlab/plot_failed_residual.m
@@ -1,24 +1,38 @@
 [n m l la nun xmin xmax ymin ymax hdim x y z xu yv zw landm] = readfort44('fort.44');
+surfm      = landm(2:n+1,2:m+1,l+1);  %Only interior surface points
+%resa = importdata('failed_rhs.atmos');
+%reso = importdata('failed_rhs.ocean');
+%reso = reso(:,3);
 
-resa = importdata('failed_rhs.atmos');
-reso = importdata('failed_rhs.ocean');
-reso = reso(:,3);
 norm(reso)
 norm(resa)
-norm([reso;resa]');
+norm([reso;resa]')
+
+L = l-5;
 
 To = zeros(m,n);
+TL = zeros(m,n);
+So = zeros(m,n);
 Ta = zeros(m,n);
 
 dim = m*n*l*nun;
 
 for j = 1:m
   for i = 1:n
-	To(m-j+1,i) = reso(find_row(nun,n,m,l,i,j,l,5));
+	To(m-j+1,i) = reso(find_row(nun,n,m,l,i,j,l,5));	
+	TL(m-j+1,i) = reso(find_row(nun,n,m,l,i,j,L,5));
+	LM(m-j+1,i) = surfm(i,j);
+	So(m-j+1,i) = reso(find_row(nun,n,m,l,i,j,l,6));
 	Ta(m-j+1,i) = resa(i + (j-1)*m);
   end
 end
 
-subplot(2,1,1); imagesc(To); colorbar
-subplot(2,1,2); imagesc(Ta); colorbar
+[j,i] = find(To==max(max(To)));
+fprintf('maximum in To at i=%d, j=%d\n', i, m+1-j);
+
+subplot(4,1,1); imagesc(To); title('surface temperature residual'); colorbar
+hold on; contour(1e-4*LM,5); hold off
+subplot(4,1,2); imagesc(TL); title(['temperature residual in layer ', num2str(L)]); colorbar
+subplot(4,1,3); imagesc(So); title('surface salinity residual'); colorbar
+subplot(4,1,4); imagesc(Ta); title('atmosphere temperature residual');colorbar
 
--- ocean/levitus.F90
+++ ocean/levitus.F90
@@ -284,7 +284,7 @@
 !
             if (weight.eq.0) then
                nmis = nmis + 1
-!w               write(f99,*)'levitus miss at :',k,i,j, nmis
+               write(*,*)'levitus miss at :',k,i,j, nmis
                iilow  = iilow - 1
                jjlow  = jjlow - 1
                iihigh = iihigh + 1
