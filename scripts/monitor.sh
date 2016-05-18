d483babf9981c9196a0563aa5fcf8d73357a9883
Erik Mulder <t.e.mulder@uu.nl>
2016-05-18 15:58:44 +0200

    Improved monitor script

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -5,7 +5,7 @@ if [ $# -eq 0 ]
 then
     echo "  using defaults..."
 	server=cartesius
-	dir=rundir_test
+	dir=run/default/
 else
 	server=$1
 	dir=$2
@@ -16,8 +16,8 @@ then
 	linhist=$3
 	newthist=$4
 else
-	linhist=5000
-	newthist=50
+	linhist=200
+	newthist=100
 fi
 
 # Random number generator
@@ -47,24 +47,23 @@ then
 	tmux split-window -h 'cd '${SHARED_DIR}'/i-emic/'$dir'/ && watch -n 5 -t ./plotresidual.sh '$linhist' '$newthist' '
 	tmux split-window -v -t 0 'exec  tail -f -n '$tailhist'  '${SHARED_DIR}'/i-emic/'$dir'/info_0.txt '
 	tmux resize-pane -L 30
-# cd '${SHARED_DIR}'/i-emic/'$dir'/; watch -n 5 -t ls '
+	
 else
 
 	tmux new-session -d -s $session_name 'exec ssh -t '$server' \
 "tail -f -n '$tailhist' \${SHARED_DIR}/i-emic/'$dir'/dump" '
-
+	
 	tmux split-window -h 'exec ssh -t '$server' "cd \${SHARED_DIR}/i-emic/'$dir'/ \
 && watch -n 5 -t ./plotresidual.sh '$linhist' '$newthist'" '
 	
 	tmux split-window -v -t 0 'exec ssh -t '$server' \
 "tail -f -n '$tailhist' \${SHARED_DIR}/i-emic/'$dir'/info_0.txt" '
-
+	
 	tmux resize-pane -L 30
 
 	tmux split-window -v -t 1 'exec ssh -t '$server' "cd \${SHARED_DIR}/i-emic/'$dir'/; bash" '
-
+	
 	tmux resize-pane -D 20
-
 fi
 
 tmux rename-window 'Status'
