b9854816b4ad582d9e2cfdf49ae59bfd134be472
Erik@Cartesius <t.e.mulder@uu.nl>
2016-05-18 15:35:57 +0200

    Improved plotscript and put scripts in the rundir

---
+++ run/paleo/65Ma3deg/paleo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --time=5-00:00:00
+#SBATCH --ntasks=48
+#SBATCH --nodes=2
+
+cd ${HOME}/Projects/i-emic/run/paleo/65Ma3deg/
+
+date=`date +%m%d%y-%k%M`
+fname=summary_$date
+fnameprev=summary_prev_$date
+infofile=info_$date
+echo writing to $fname
+procs=48
+
+echo "Global configuration on:" ${PLAT} " in:" ${PWD} > $fname
+echo "#Procs:" $procs >> $fname
+echo "#Procs:" $procs 
+
+# backup old data
+cat profile_output > $fnameprev
+cat *.plot >> $fnameprev
+cp info_0.txt $infofile
+
+# run
+srun -n $procs run_ocean > dump
+
+cat profile_output >> $fname
--- scripts/plotresidual.sh
+++ scripts/plotresidual.sh
@@ -16,7 +16,6 @@ horizontal=$((`tput cols` - 5))
 vertical=$((`tput lines` * 2 / 5))
 padding=$(( (`tput lines` - 2 * $vertical) / 2 ))
 
-
 for i in `eval echo {1..$padding}`
 do
     if [ $i -eq $(( $padding / 2 )) ]
@@ -30,28 +29,39 @@ done
 # Two possibilities for extracting linear solver residuals:
 less dump | grep 'impl res' | tail -n $solvehistory | sed 's/.*iteration://' | sed 's/impl res://' \
 	| sed 's/expl.*//' > solveresiduals
-less dump | grep 'residual \[ 0 \]' | tail -n $solvehistory | sed 's/.*=\ //' | sed 's/\ >.*//' \
-	> solveresiduals
+column=2
+
+if ! [[ -s "solveresiduals" ]]
+then
+	less dump | grep 'residual \[ 0 \]' | tail -n $solvehistory | sed 's/.*=\ //' | sed 's/\ >.*//' \
+		> solveresiduals
+	column=1
+fi
 
 # Extract Newton solve residuals
 less info_0.txt | grep '||R||' | tail -n $newtonhistory | sed 's/.*||R||://' > newtonresiduals
+minimumsize=100
+actualsize=$(wc -c < solveresiduals)
 
-if [ -s "solveresiduals" ]
+if [[ $actualsize -ge $minimumsize ]]
 then
-	gnuplot -e "set terminal dumb $horizontal $vertical;\
+	if [[ -s "solveresiduals" ]]
+	then
+		gnuplot -e "set terminal dumb $horizontal $vertical;\
                 set logscale y;\
                 unset xtics;\
                 set title 'linear solver';\
-                plot 'solveresiduals' using 2 with lines linetype 0 notitle;" > solveresidual.plot
-	cat solveresidual.plot
-fi
+                plot 'solveresiduals' using $column with lines linetype 0 notitle;" > solveresidual.plot
+		cat solveresidual.plot
+	fi
 
-if [ -s "newtonresiduals" ]
-then
-	gnuplot -e "set terminal dumb $horizontal $vertical;\
+	if [[ -s "newtonresiduals" ]]
+	then
+		gnuplot -e "set terminal dumb $horizontal $vertical;\
                 set logscale y;\
                 unset xtics;\
                 set title 'Newton iterations';\
                 plot 'newtonresiduals' using 1 with lines linetype 0 notitle" > newtonresidual.plot
-	cat newtonresidual.plot
+		cat newtonresidual.plot
+	fi
 fi
