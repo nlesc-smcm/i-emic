727437fde223f578edbed9bf20c924a08ad9c7fa
erik_laptop <erik.mulder@gmail.com>
2016-06-10 20:14:58 +0200

    Problem is probably coupling between u,v, and p

--- test/masks_light/test_mask.m
+++ test/masks_light/test_mask.m
@@ -4,11 +4,8 @@ end
 
 [N,M] = size(A);
 
-[n m l la nun xmin xmax ymin ymax hdim x y z xu yv zw landm] = readfort44('fort.44');
-
-n = 120;
-m = 54;
-l = 12;
+[n m l la nun xmin xmax ymin ymax hdim x y z xu yv zw landm] = ...
+readfort44('fort.44');
 
 assert(N == n*m*l*nun);
 
@@ -16,20 +13,11 @@ assert(N == n*m*l*nun);
 iu  = 1:6:N;
 iv  = 2:6:N;
 iw  = 3:6:N;
-ipt = 4:6:N;
-ip  = [];
-rp  = [];
+ip = 4:6:N;
 
+ZR  = load('zerorows');
 SWS = load('singrows');
-fprintf('building p indices...\n')
-tic;
-for i = 1:numel(SWS)
-  if SWS(i) == 0
-	 ip = [ip, ipt(i)];
-	 rp = [rp, i];
-  end
-end
-fprintf('building p indices... done %d\n', toc)
+
 iuv = sort([iu,iv]);
 
 Auv  = A(iuv,iuv);
@@ -40,48 +28,36 @@ if ~exist('iAuv') || force
   iAuv = inverseblockdiagonal(Auv,2);
 end
 
-E  = -Duv * Guv;
-dE = diag(E);
-for i = 1:numel(dE)
-  if (abs(dE(i)) > 3000)
-	fprintf('%6d  ', i);
-	fprintf('%1.3e ', sum(full(Duv(i,:))));
-	fprintf('\n');
-	SWS(rp(i)) = 6;
-  end
-end
-		 
 C   = -Duv * iAuv * Guv;
-
-C11 = C(rp<n*m,rp<n*m);
-E11 = E(rp<n*m,rp<n*m);
-
-condest(C11)
-
-figure(1)
-plot(diag(E11))
-sc    = sum(abs(E11),2);
-E11sc = diag(1./sc)*E11;
-
-figure(2)
-plot(diag(E11sc));
-condest(E11sc)
-
-figure(3)
 
 c = diag(C);
 z = zeros(numel(c),1);
 for i = 1:numel(c);
-  if (sum(abs(c(i))) < 1e-10)
+  if (sum(abs(c(i))) < 1e-3)
 	z(i) = 1;
   end  
 end
+dc11 = c(1:m*n);
+DC11 = reshape(dc11, m, n);
+SA11 = full(sum(A(iv(1:m*n),:),2));
+SA11 = reshape(SA11, m, n);
+
+figure(1)
+imagesc(DC11')
+set(gca,'ydir','normal')
+
+figure(2)
+imagesc(SA11')
+set(gca,'ydir','normal')
+
+figure(3)
+plot(c(1:m*n))
 
 fprintf('----------------------\n');
 for i = 1:numel(ip)
-  %fprintf('%3d      %3d %3d\n', i, z(i), SWS(rp(i)));
-  if z(i) == 1 && SWS(rp(i)) == 0
-	SWS(rp(i)) = 3;
+  fprintf('%3d      %3d %3d\n', i, z(i), SWS(i));
+  if z(i) == 1 && SWS(i) == 0
+	SWS(i) = 3;
   end
 end
 
