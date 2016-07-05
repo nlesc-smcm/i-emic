ef3bf54b2664b2a754be28353b49736bbb03f99f
Erik Mulder <t.e.mulder@uu.nl>
2016-07-05 15:02:07 +0200

    Implemented minimum newton iterations  got rid of ceiling, not sure why we use it. It's weird.

--- matlab/plot_failed_residual.m
+++ matlab/plot_failed_residual.m
@@ -9,8 +9,8 @@ surfm = landm(2:n+1,2:m+1,l+1);  %Only interior surface points
 
 %resa = load('failed_rhs.atmos');
 %reso = load('stateDot.ocean');
-%reso = load('xdot.ocean');
-reso = load('stateDir.ocean');
+reso = load('xdot.ocean');
+%reso = load('stateDir.ocean');
 
 % pargrid line color
 pcol = [.3 .3 .3];
