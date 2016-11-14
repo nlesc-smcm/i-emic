09680625427951940cd961d3d911d3a7d09719c2
erik@laptop <t.e.mulder@uu.nl>
2016-11-14 15:21:02 +0100

    Build infrastructure to follow transports

--- matlab/compute_transports.m
+++ matlab/compute_transports.m
@@ -1,15 +1,17 @@
-function [] = compute_transports(solfile, datafile, use_mouse)
+function [path] = compute_transports(solfile, datafile, trpath)
 
+  transmode = 2;
   if nargin < 3
-	use_mouse = true;
-	if nargin < 2
-	  datafile = 'fort.44';
-	  if nargin < 1
-		solfile = 'fort.3';
-	  end
-	end
+	transmode = 1;
+	fprintf('Using mouse input to define transect/path\n');
+	trpath = [];
   end
-  
+  if nargin < 2
+	datafile = 'fort.44';
+  end
+  if nargin < 1
+	solfile = 'fort.3';
+  end	
 
   [n m l la nun xmin xmax ymin ymax hdim x y z xu yv zw landm] = ...
   readfort44(datafile);
@@ -24,7 +26,7 @@ function [] = compute_transports(solfile, datafile, use_mouse)
 
   %% - READ SOLUTION - -------------------------------------------------
   [lab icp par xl xlp det sig sol solup soleig] = ...
-  readfort3(la, solfile);
+  readfort3(la, solfile); 
 
   %% - EXTRACT SOLUTION COMPONENTS - -----------------------------------
   [u,v,w,p,T,S] = extractsol(sol);
@@ -37,31 +39,32 @@ function [] = compute_transports(solfile, datafile, use_mouse)
   S0    = 35;                  %[psu]   Reference salinity
   RtD   = 180/pi;              %[-]     Radians to degrees  
 
+  
   %% - Obtain coordinates for transect
-  coords = getcoordinates(use_mouse, tmp_maskp);
-  
-  path = getpath(coords, int_maskp);
-
-  % - Display path
-  for i = 1:size(path,1);
-	int_maskp(path(i,2),path(i,1)) = l+4*path(i,3);
+  if transmode == 1
+	coords = getcoordinates(tmp_maskp);  
+	trpath = getpath(coords, int_maskp);
+	
+	%% - Display path with streamfunction
+	for i = 1:size(trpath,1);
+	  int_maskp(trpath(i,2),trpath(i,1)) = l+4*trpath(i,3);
+	end
+	
+	imagesc(RtD*x,RtD*(y),int_maskp); set(gca,'ydir','normal');
+	caxis([0,2*l])
+	hold on
+	PSIB = bstream(u*udim,zw*hdim,[y;ymax]*r0dim);
+	img = PSIB(2:end,:)';
+	contour(RtD*x,RtD*(y),img,40,'linewidth',.5);
+	hold off
   end
-  imagesc(RtD*x,RtD*(y),int_maskp); set(gca,'ydir','normal');
-  caxis([0,2*l])
   
-  % - Compute transport for path
-  transport = compute_transport(path, u*udim, v*udim, ...
+  %% - Compute transport for path
+  transport = compute_transport(trpath, u*udim, v*udim, ...
 								[x;xmax]*cos(yv(2:m+1))'*r0dim, ...
 								[y;ymax]*r0dim, zw*hdim);
 
-  disp(transport)
-  
-  hold on
-  PSIB = bstream(u*udim,zw*hdim,[y;ymax]*r0dim);
-  img = PSIB(2:end,:)';
-  contour(RtD*x,RtD*(y),img,40,'linewidth',.5);
-  hold off
-  
+  fprintf('transport through selected section: %f\n', transport);
 end
 
 function [transport] = compute_transport(path, u, v, x, y, z)
@@ -73,140 +76,28 @@ function [transport] = compute_transport(path, u, v, x, y, z)
   for i = 1:size(path,1)	  
 
 	if path(i,3) == 1 % u-orientation
-	  % integrate u in depth
+					  % integrate u in depth
 	  for j = 1:size(z)-1
 		vel(i) = vel(i) + u(path(i,1),path(i,2),j) * (z(j+1)-z(j));
 	  end
-	  % append y grid increment
+								% append y grid increment
 	  vel(i) = vel(i) * dy;
       
     elseif path(i,3) == 2 % v-orientation
-      % integrate v in depth
+						  % integrate v in depth
       for j = 1:size(z)-1
     	vel(i) = vel(i) + v(path(i,1),path(i,2),j) * (z(j+1)-z(j));
       end
-      % append x grid increment
+								% append x grid increment
 	  dx = x(path(i,1)+1,path(i,2))-x(path(i,1),path(i,2));
       vel(i) = vel(i) * dx;
     end
 
   end
 
-  % integrate over path and convert to Sverdrups
+						% integrate over path and convert to Sverdrups
   transport = sum(vel)/1.e6; 
 end
 
-function [path] = getpath(coords, mask)
-  x1 = coords(1,1);
-  y1 = coords(1,2);
-  x2 = coords(2,1);
-  y2 = coords(2,2);
 
-  % starting point
-  x = x1;
-  y = y1;
-
-  % difference with target
-  dx = (x - x2) / max(abs(x - x2),1);
-  dy = (y - y2) / max(abs(y - y2),1);
-
-  % initialize list that stores the path and orientation
-  % orientation: or = 1 : u-direction
-  %              or = 2 : v-direction
-
-  or = 2;
-  if dx == 0
-	or = 1;
-  end
-
-  if 1%(mask(y,x) > 0)
-	path = [x,y,or];
-  else
-	path = [];
-  end
-
-  corner = false;
-  while (dx ~= 0) || (dy ~= 0)
-
-	% traverse the transect, first in the x-direction
-	if dx ~= 0
-	  switch dx
-		case 1
-		  x = x - 1;
-		case -1 
-		  x = x + 1;
-	  end
-	  dx = (x - x2) / max(abs(x - x2),1);
-	  or = 2;
-	  if (dx == 0) && (dy ~= 0) 
-		corner = true;
-	  end
-
-	% y-direction if x is aligned with x2
-	elseif dy ~= 0
-		   
-	  switch dy
-		case 1
-		  y = y - 1;
-		case -1 
-		  y = y + 1;
-	  end
-	  dy = (y - y2) / max(abs(y - y2),1);
-	  or = 1;
-	  corner = false;
-	end
-	
-	if ~corner && 1%(mask(y,x) > 0)
-	  path = [path;x,y,or];
-	end
-  end
-end
-
-function [coords] = getcoordinates(use_mouse, mask)
-  if use_mouse
-
-	reset_mask = mask;
-	fprintf('Usage: \n')
-	fprintf('  Left mouse:    set coordinate\n')
-	fprintf('  Right mouse:   retry\n')
-	fprintf('  Middle mouse:  abort\n')
-
-	coords = [];
-	mx = max(max(mask));
-	while size(coords,1) < 2
-		  
-	  imagesc(mask); set(gca,'ydir','normal');
-	  
-	  [x,y,button] = ginput(1);
-	  x = round(x);
-	  y = round(y);
-	  if (button == 1)
-		mask(y,x) = mx+8;
-		coords = [coords;x,y];
-	  elseif (button == 3)
-		continue;
-	  elseif (button == 2)
-		break;
-	  end
-
-	  if size(coords,1) == 2
-		d = coords(2,:) - coords(1,:);
-		if d(1) ~= 0 && d(2) ~= 0
-		  fprintf('We only allow straight meridional and zonal transects!\n')
-		  fprintf('Resetting...\n');
-		  mask = reset_mask;
-		  coords = [];
-		end
-	  end
-	end
-	
-	
-	imagesc(mask); set(gca,'ydir','normal');
-	pause(.2);
-  else
-	coords = [5,9;5,16]; % test coords
-  end
-
-  assert(norm(size(coords)-[2,2])==0);
-end
 
--- matlab/transform_mask.m
+++ matlab/transform_mask.m
@@ -1,8 +1,11 @@
 function [peri, pmask] = transform_mask(mask_name, periodic)
-  % This function transforms Michiel's masks to THCM masks
-  % Supply the name of the .mat file and whether you want periodic
-  % boundaries. 
-		 
+
+	  % transform_mask(mask_name, periodic)
+	  % 
+	  % This function transforms Michiel's masks to THCM masks
+	  % Supply the name of the .mat file and whether you want periodic
+	  % boundaries. 
+  
   if nargin < 2
 	periodic = true;
   end
@@ -19,10 +22,10 @@ function [peri, pmask] = transform_mask(mask_name, periodic)
   mask     =  ~mimport;
   [m,n,l]  =  size(mask)
 
-  % check whether we have a non-layered format
+						  % check whether we have a non-layered format
   if (l == 1)
 	fprintf('  not in layered format: converting!\n');
-	% convert to layered format
+								% convert to layered format
 	l = max(max(abs(mimport)))
 	mask = zeros(m,n,l);
 	for i = 1:l
@@ -30,15 +33,15 @@ function [peri, pmask] = transform_mask(mask_name, periodic)
 	end
   end
 
-  % Create file id 
+								% Create file id 
   fid      =  fopen([mask_name, '.mask'],'w');
   
-  % set padded mask pmask  
+								% set padded mask pmask  
   pmask = ones(m+2,n+2,l+2);
   pmask(2:m+1,2:n+1,2:l+1) = mask;
 
   if (periodic)
-	% find and set periodic boundaries
+								% find and set periodic boundaries
 	for k = 1:l
 	  peri = mask(:,1,k) + mask(:,n,k);
 	  peri = logical([0; squeeze(logical(peri == 0)); 0]);
@@ -47,7 +50,7 @@ function [peri, pmask] = transform_mask(mask_name, periodic)
 	end
   end
   
-  % write padded mask to file
+								% write padded mask to file
   fprintf('  writing to %s\n',[mask_name, '.mask']);
   
   for k = l+2:-1:1
