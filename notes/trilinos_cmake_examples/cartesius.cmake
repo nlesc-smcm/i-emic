2ddc4fd558da232a6029abd61c556a3c99c85259
erik@laptop <t.e.mulder@uu.nl>
2017-02-07 11:08:41 +0100

    added trilinos cmake notes

---
+++ notes/trilinos_cmake_examples/cartesius.cmake
@@ -0,0 +1,50 @@
+#!/bin/bash                                                                                               
+rm -rf CMakeCache.txt
+rm -rf CMakeFiles
+
+module load cmake/3.0.1
+module load mkl
+MKL_LIBS="mkl_intel_lp64;mkl_intel_thread;mkl_core;pthread"
+
+cmake \
+  -D CMAKE_INSTALL_PREFIX:PATH=${HOME}/trilinos/11.14 \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D BUILD_SHARED_LIBS:BOOL=ON \
+\
+  -D TPL_ENABLE_MPI:BOOL=ON \
+  -D TPL_ENABLE_MKL:BOOL=ON \
+  -D TPL_ENABLE_BLAS:BOOL=ON \
+  -D TPL_ENABLE_LAPACK:BOOL=ON \
+  -D TPL_ENABLE_Pthread:BOOL=ON \
+  -D TPL_ENABLE_HWLOC:BOOL=ON \
+\
+  -D TPL_MPI_LIBRARIES:STRING=${MKL_LIBS} \
+  -D TPL_MKL_LIBRARIES=${MKL_LIBS} \
+  -D TPL_BLAS_LIBRARIES:STRING=${MKL_LIBS} \
+  -D TPL_LAPACK_LIBRARIES:STRING=${MKL_LIBS} \
+  -D TPL_MKL_INCLUDE_DIRS=${SURFSARA_MKL_INCLUDE} \
+\
+  -D TPL_ENABLE_METIS:BOOL=ON \
+  -D METIS_LIBRARY_DIRS:PATH=${HOME}/metis/lib \
+  -D TPL_METIS_INCLUDE_DIRS:PATH=${HOME}/metis/include \
+  -D TPL_ENABLE_ParMETIS:BOOL=ON \
+  -D ParMETIS_LIBRARY_DIRS:PATH=${HOME}/parmetis/lib \
+  -D TPL_ParMETIS_INCLUDE_DIRS:PATH=${HOME}/parmetis/include \
+\
+  -D CMAKE_C_COMPILER:STRING="mpiicc" \
+  -D CMAKE_CXX_COMPILER:STRING="mpiicpc" \
+  -D CMAKE_Fortran_COMPILER:STRING="mpiifort" \
+  -D CMAKE_CXX_FLAGS:STRING="-O3 -g -fopenmp -DMPICH_SKIP_MPICXX" \
+  -D Ifpack_CXX_FLAGS:STRING="-DHAVE_IFPACK_MRILU"\
+\
+  -D Trilinos_ENABLE_Export_Makefiles:BOOL=ON \
+  -D EpetraExt_USING_HDF5:BOOL=ON \
+\
+  -D Trilinos_ENABLE_ML:BOOL=ON \
+  -D Trilinos_ENABLE_Teuchos:BOOL=ON \
+  -D Trilinos_ENABLE_Belos:BOOL=ON \
+  -D Trilinos_ENABLE_Epetra:BOOL=ON \
+  -D Trilinos_ENABLE_Ifpack:BOOL=ON \
+  -D Trilinos_ENABLE_Amesos:BOOL=ON \
+  -D Trilinos_ENABLE_Anasazi:BOOL=ON \
+../
---
+++ notes/trilinos_cmake_examples/ubuntu.cmake
@@ -0,0 +1,38 @@
+rm -rf CMakeCache.txt CMakeFiles
+
+cmake \
+   -D CMAKE_INSTALL_PREFIX:PATH=/home/erik/trilinos/11.12/dynamic \
+   -D TPL_ENABLE_MPI:BOOL=ON \
+   -D TPL_ENABLE_METIS:BOOL=ON \
+   -D METIS_LIBRARY_DIRS:PATH=/usr/local/lib \
+   -D TPL_METIS_INCLUDE_DIRS:PATH=/usr/local/include \
+   -D TPL_ENABLE_ParMETIS:BOOL=ON \
+   -D ParMETIS_LIBRARY_DIRS:PATH=/usr/local/lib \
+   -D TPL_ParMETIS_INCLUDE_DIRS:PATH=/usr/local/include \
+\
+   -D Trilinos_VERBOSE_CONFIGURE:BOOL=OFF \
+   -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+\
+   -D CMAKE_BUILD_TYPE:STRING=RELEASE \
+   -D BUILD_SHARED_LIBS=ON \
+   -D CMAKE_CXX_FLAGS:STRING="-g -O3" \
+   -D Ifpack_CXX_FLAGS:STRING="-DHAVE_IFPACK_MRILU" \
+   -D Ifpack_CXX_FLAGS:STRING="-I/home/erik/Projects/i-emic/src/mrilucpp" \
+   -D Trilinos_ENABLE_DEBUG=OFF \
+\
+   -D Trilinos_ENABLE_Export_Makefiles:BOOL=ON \
+   -D Trilinos_ENABLE_EXAMPLES:BOOL=OFF \
+   -D Trilinos_ENABLE_TESTS:BOOL=OFF \
+   -D EpetraExt_USING_HDF5:BOOL=ON \
+   -D Belos_ENABLE_Experimental:BOOL=OFF \
+\
+   -D Trilinos_ENABLE_Teuchos:BOOL=ON \
+   -D Trilinos_ENABLE_Epetra:BOOL=ON \
+   -D Trilinos_ENABLE_EpetraExt:BOOL=ON \
+   -D Trilinos_ENABLE_Ifpack:BOOL=ON \
+   -D Trilinos_ENABLE_Amesos:BOOL=ON \
+   -D Trilinos_ENABLE_Anasazi:BOOL=ON \
+   -D Trilinos_ENABLE_Belos:BOOL=ON \
+   -D Trilinos_ENABLE_ML:BOOL=ON \
+\
+../
