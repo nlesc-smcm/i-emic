0b21529a0d968ef642bdfd04a09f557d7034e23a
erik@laptop <t.e.mulder@uu.nl>
2018-01-24 22:00:56 +0100

    Coupled jdqz solver to continuation. Need to test interface.

--- src/main/run_coupled.C
+++ src/main/run_coupled.C
@@ -8,6 +8,8 @@
 using Teuchos::RCP;
 using Teuchos::rcp;
 
+using JDQZsolver = JDQZ<JDQZInterface<std::shared_ptr<CoupledModel>, 
+                                      ComplexVector<Combined_MultiVec> > >;
 
 void runCoupledModel(RCP<Epetra_Comm> Comm);
 
@@ -43,30 +45,28 @@ void runCoupledModel(RCP<Epetra_Comm> Comm)
     if (outFile == Teuchos::null)
         throw std::runtime_error("ERROR: Specify output streams");
 
-
     // First we create a bunch of parameterlists
     
     // Create parameter object for Ocean
-    RCP<Teuchos::ParameterList> oceanParams = rcp(new Teuchos::ParameterList);
-    updateParametersFromXmlFile("ocean_params.xml", oceanParams.ptr());
-    oceanParams->setName("Ocean parameters");
+    RCP<Teuchos::ParameterList> oceanParams =
+        obtainParams("ocean_params", "Ocean parameters");
 
     // Create parameter object for Atmosphere
-    RCP<Teuchos::ParameterList> atmosphereParams = rcp(new Teuchos::ParameterList);
-    updateParametersFromXmlFile("atmosphere_params.xml", atmosphereParams.ptr());
-    atmosphereParams->setName("Atmosphere parameters");
+    RCP<Teuchos::ParameterList> atmosphereParams =
+        obtainParams("atmosphere_params.xml", "Atmosphere parameters"); 
 
     // Create parameter object for CoupledModel
-    RCP<Teuchos::ParameterList> coupledmodelParams = rcp(new Teuchos::ParameterList);
-    updateParametersFromXmlFile("coupledmodel_params.xml", coupledmodelParams.ptr());
-    coupledmodelParams->setName("CoupledModel parameters");
+    RCP<Teuchos::ParameterList> coupledmodelParams =
+        obtainParams("coupledmodel_params.xml", "CoupledModel parameters"); 
 
     // Create parameter object for Continuation
-    RCP<Teuchos::ParameterList> continuationParams = rcp(new Teuchos::ParameterList);
-    updateParametersFromXmlFile("continuation_params.xml", continuationParams.ptr());
-    continuationParams->setName("Continuation parameters");
+    RCP<Teuchos::ParameterList> continuationParams =
+        obtainParams("continuation_params.xml", "Continuation parameters"); 
 
-    INFO("Overwriting:");
+    // Create parameter object for JDQZ
+    RCP<Teuchos::ParameterList> jdqzParams =
+        obtainParams("jdqz_params.xml", "JDQZ parameters"); 
+
     // The Continuation and CoupledModel parameterlists overwrite settings
     Utils::overwriteParameters(oceanParams,        coupledmodelParams);
     Utils::overwriteParameters(atmosphereParams,   coupledmodelParams);
@@ -74,7 +74,6 @@ void runCoupledModel(RCP<Epetra_Comm> Comm)
     Utils::overwriteParameters(oceanParams,        continuationParams);
     Utils::overwriteParameters(atmosphereParams,   continuationParams);
     Utils::overwriteParameters(coupledmodelParams, continuationParams);
-    
     
     // Create parallelized Ocean object
     std::shared_ptr<Ocean> ocean = std::make_shared<Ocean>(Comm, oceanParams);
@@ -87,15 +86,28 @@ void runCoupledModel(RCP<Epetra_Comm> Comm)
     std::shared_ptr<CoupledModel> coupledModel =
         std::make_shared<CoupledModel>(ocean, atmos, coupledmodelParams);
 
+    // Create JDQZ generalized eigenvalue solver
+    Combined_MultiVec t = *coupledModel->getSolution('C');
+    t.PutScalar(0.0);
+    ComplexVector<Combined_MultiVec> z(t);
+    
+    JDQZInterface<std::shared_ptr<CoupledModel>,
+                  ComplexVector<Combined_MultiVec> > matrix(coupledModel, z);
+    
+    std::shared_ptr<JDQZsolver> jdqz = std::make_shared<JDQZsolver>(matrix, z);
+    jdqz->setParameters(*jdqzParams);
+    
     // Create Continuation
     Continuation<std::shared_ptr<CoupledModel>, RCP<Teuchos::ParameterList> >
         continuation(coupledModel, continuationParams);
+
+    // Couple JDQZ to continuation
+    continuation.setEigenSolver(jdqz);
 
     TIMER_STOP("Total initialization");
 
     // Run continuation
     continuation.run();
-
-    //------------------------------------------------------------------
+    
     TIMER_STOP("Total time...");
 }
