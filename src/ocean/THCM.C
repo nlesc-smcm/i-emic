fa699acf7be0e9fc20af2b863af6aff843e573bb
Erik@Cartesius <t.e.mulder@uu.nl>
2018-03-28 08:39:50 +0200

    Disabliing the pressure point fixing, need to test that first and add an xml option.

--- src/ocean/THCM.C
+++ src/ocean/THCM.C
@@ -678,11 +678,13 @@ THCM::THCM(Teuchos::ParameterList& params, Teuchos::RCP<Epetra_Comm> comm) :
 //      int M = domain->GlobalM();
 //      int L = domain->GlobalL();
 //
-    rowPfix1 = FIND_ROW2(_NUN_,N,M,L,N-1,M-1,L-1,PP);
-    rowPfix2 = FIND_ROW2(_NUN_,N,M,L,N-2,M-1,L-1,PP);
 
-    // rowPfix1=-1;
-    // rowPfix2=-1;
+    // FIXME this needs an xml option
+    // rowPfix1 = FIND_ROW2(_NUN_,N,M,L,N-1,M-1,L-1,PP);
+    // rowPfix2 = FIND_ROW2(_NUN_,N,M,L,N-2,M-1,L-1,PP);
+
+    rowPfix1=-1;
+    rowPfix2=-1;
 
     // build vectonr with integral coefficients
     this->evaluateB();
@@ -931,8 +933,9 @@ bool THCM::evaluate(const Epetra_Vector& soln,
             this->intcond_S(*localJac,*localDiagB);
         }
 #endif
-        
-        this->fixPressurePoints(*localJac,*localDiagB);
+
+        // FIXME this needs an xml option
+        // this->fixPressurePoints(*localJac,*localDiagB);
         CHECK_ZERO(localJac->FillComplete());
 
         // redistribute according to SolveMap (may be load-balanced)
