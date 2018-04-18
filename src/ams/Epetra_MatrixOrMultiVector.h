b2324ed9c14ea5970cb5c1a08b5ed2018aef6f82
Sven Baars <s.baars@rug.nl>
2018-04-18 14:54:22 +0200

    AMS: Move factorization to Epetra_MatrixOrMultiVector

--- src/ams/Epetra_MatrixOrMultiVector.h
+++ src/ams/Epetra_MatrixOrMultiVector.h
@@ -7,15 +7,20 @@
 #include "Epetra_SerialComm.h"
 #include "Epetra_MultiVector.h"
 #include "Epetra_SerialDenseMatrix.h"
+#include "Epetra_SerialDenseSolver.h"
 
 class Epetra_MatrixOrMultiVector: public Epetra_MultiVector
 {
+    bool factored;
+    Teuchos::RCP<Epetra_SerialDenseMatrix> fac;
+    Teuchos::RCP<Epetra_SerialDenseSolver> sol;
 public:
     Teuchos::RCP<Epetra_SerialDenseMatrix> mat;
 
     Epetra_MatrixOrMultiVector(int m, int n)
         :
         Epetra_MultiVector(Epetra_LocalMap(m, 0, Epetra_SerialComm()), n),
+        factored(false),
         mat(Teuchos::rcp(new Epetra_SerialDenseMatrix(
                              View, Values(), Stride(), MyLength(), NumVectors())))
         {}
@@ -24,6 +29,7 @@ public:
         :
         Epetra_MultiVector(Copy, Epetra_LocalMap(in.M(), 0, Epetra_SerialComm()),
                            in.A(), in.LDA(), in.N()),
+        factored(false),
         mat(Teuchos::rcp(new Epetra_SerialDenseMatrix(
                              View, Values(), Stride(), MyLength(), NumVectors())))
         {}
@@ -31,6 +37,7 @@ public:
     Epetra_MatrixOrMultiVector(Epetra_MultiVector const &in)
         :
         Epetra_MultiVector(in),
+        factored(false),
         mat(Teuchos::rcp(new Epetra_SerialDenseMatrix(
                              View, Values(), Stride(), MyLength(), NumVectors())))
         {}
@@ -38,6 +45,7 @@ public:
     Epetra_MatrixOrMultiVector(Epetra_MatrixOrMultiVector const &in)
         :
         Epetra_MultiVector(in),
+        factored(false),
         mat(Teuchos::rcp(new Epetra_SerialDenseMatrix(
                              View, Values(), Stride(), MyLength(), NumVectors())))
         {}
@@ -45,6 +53,35 @@ public:
     Epetra_MatrixOrMultiVector() = delete;
 
     ~Epetra_MatrixOrMultiVector() {}
+
+    int Factor()
+        {
+            int ierr = 0;
+            sol = Teuchos::rcp(new Epetra_SerialDenseSolver());
+            fac = Teuchos::rcp(new Epetra_SerialDenseMatrix(*mat));
+            ierr = sol->SetMatrix(*fac);
+            EPETRA_CHK_ERR(ierr);
+            ierr = sol->Invert();
+            EPETRA_CHK_ERR(ierr);
+            factored = true;
+            return ierr;
+        }
+
+    int ApplyInverse(Epetra_MatrixOrMultiVector const &X, Epetra_MatrixOrMultiVector &Y)
+        {
+            int ierr = 0;
+            if (!factored)
+            {
+                ierr = Factor();
+                EPETRA_CHK_ERR(ierr);
+            }
+
+            ierr = sol->SetVectors(*Y.mat, *X.mat);
+            EPETRA_CHK_ERR(ierr);
+
+            ierr = sol->Solve();
+            return ierr;
+        }
 };
 
 #endif
