441a7908adc5153af5790313ddc562592369d728
erik@laptop <t.e.mulder@uu.nl>
2018-06-01 15:27:28 +0200

    * src/ocean/ice.F90: adding ice module to initialize a few params * src/ocean/CMakeLists.txt:

---
+++ src/ocean/ice.F90
@@ -0,0 +1,12 @@
+
+module m_ice
+  
+  real, parameter :: zeta ! combination of sea ice parameters
+  real, parameter :: a0   ! freezing temperature S sensitivity
+  real, parameter :: Lf   ! latent heat of fusion of ice
+
+  
+contains
+
+
+end module m_ice
