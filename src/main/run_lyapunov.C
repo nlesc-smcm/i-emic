04ec814b951c2f407390d1f4079b9be3b33c9907
Sven Baars <s.baars@rug.nl>
2018-07-20 12:06:01 +0200

    Lyapunov: Add a Lyapunov solver model

---
+++ src/main/run_lyapunov.C
@@ -0,0 +1,114 @@
+//=======================================================================
+// Main continuation of the Lyapunov model
+//=======================================================================
+
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_XMLParameterListHelpers.hpp>
+
+#include <memory>
+
+#include "GlobalDefinitions.H"
+#include "Utils.H"
+
+#include "ComplexVector.H"
+#include "JDQZInterface.H"
+#include "jdqz.hpp"
+
+#include "Continuation.H"
+#include "Ocean.H"
+#include "LyapunovModel.H"
+
+//------------------------------------------------------------------
+using Teuchos::RCP;
+using Teuchos::rcp;
+
+using JDQZsolver = JDQZ<JDQZInterface<Teuchos::RCP<LyapunovModel<Ocean> >,
+                                      ComplexVector<Epetra_Vector> > >;
+
+//------------------------------------------------------------------
+void runOceanModel(RCP<Epetra_Comm> Comm);
+
+//------------------------------------------------------------------
+int main(int argc, char **argv)
+{
+    // Initialize the environment:
+    //  - MPI
+    //  - output files
+    //  - returns Trilinos' communicator Epetra_Comm
+    RCP<Epetra_Comm> Comm = initializeEnvironment(argc, argv);
+
+    // run the ocean model
+    runOceanModel(Comm);
+
+    //--------------------------------------------------------
+    // Finalize MPI
+    //--------------------------------------------------------
+    MPI_Finalize();
+}
+
+//------------------------------------------------------------------
+void runOceanModel(RCP<Epetra_Comm> Comm)
+{
+    TIMER_START("Total time...");
+
+    //------------------------------------------------------------------
+    // Check if outFile is specified
+    if (outFile == Teuchos::null)
+        throw std::runtime_error("ERROR: Specify output streams");
+
+    // Create parameter object for Ocean
+    RCP<Teuchos::ParameterList> oceanParams = rcp(new Teuchos::ParameterList);
+    updateParametersFromXmlFile("ocean_params.xml", oceanParams.ptr());
+    oceanParams->setName("Ocean parameters");
+
+    // Create parameter object for continuation
+    RCP<Teuchos::ParameterList> continuationParams = rcp(new Teuchos::ParameterList);
+    updateParametersFromXmlFile("continuation_params.xml", continuationParams.ptr());
+    continuationParams->setName("Continuation parameters");
+
+    // Create parameter object for JDQZ
+    RCP<Teuchos::ParameterList> jdqzParams =
+        Utils::obtainParams("jdqz_params.xml", "JDQZ parameters");
+
+    // Let the continuation parameters dominate over ocean parameters
+    Utils::overwriteParameters(oceanParams, continuationParams);
+
+    // Create parallelized Ocean object
+    RCP<LyapunovModel<Ocean> > ocean = Teuchos::rcp(
+        new LyapunovModel<Ocean>(Comm, oceanParams));
+
+    // Create continuation
+    Continuation<RCP<LyapunovModel<Ocean> >, RCP<Teuchos::ParameterList> >
+        continuation(ocean, continuationParams);
+
+    // Create JDQZ generalized eigenvalue solver:
+    // 1) Create a vector with complex arithmetic based on an Epetra_Vector
+    Epetra_Vector t = *ocean->getSolution('C');
+    t.PutScalar(0.0);
+    ComplexVector<Epetra_Vector> z(t);
+
+    // 2) Create JDQZ (mass) matrix and preconditioning interface
+    JDQZInterface<Teuchos::RCP<LyapunovModel<Ocean> >,
+                  ComplexVector<Epetra_Vector> > matrix(ocean, z);
+
+    // 3) Create JDQZ solver
+    std::shared_ptr<JDQZsolver> jdqz = std::make_shared<JDQZsolver>(matrix, z);
+    jdqz->setParameters(*jdqzParams);
+
+    // Couple JDQZ to continuation
+    continuation.setEigenSolver(jdqz);
+
+    // Run continuation
+    int status = continuation.run();
+    assert(status == 0);
+
+    TIMER_STOP("Total time...");
+
+    // print the profile
+    if (Comm->MyPID() == 0)
+    {
+        printProfile();
+        jdqz->printProfile("jdqz_profile");
+    }
+}
+
