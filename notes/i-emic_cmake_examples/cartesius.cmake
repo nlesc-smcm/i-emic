ada06061be31c2857e50bc2f197209e55ef82274
Erik@Cartesius <t.e.mulder@uu.nl>
2018-08-07 21:41:33 +0200

    slightly streamlined build process on cartesius... not there yet

--- notes/i-emic_cmake_examples/cartesius.cmake
+++ notes/i-emic_cmake_examples/cartesius.cmake
@@ -1,3 +1,8 @@
 rm -rf CMakeCache.txt CMakeFiles
 
-cmake ../	
+cmake \
+-DMRILU_DIR=/home/emulder/mrilu/ifort \
+-DTrilinos_DIR=/home/emulder/trilinos/11.14 \
+-DGTEST_DIR=/hpc/sw/gtest-1.7.0/ \
+\
+../
