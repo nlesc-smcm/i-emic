31a9eb75bd1dbf2bbb8f2e11451e6d8aa1ec0a9c
erik@laptop <t.e.mulder@uu.nl>
2018-09-04 17:09:23 +0200

    Revert "slightly streamlined build process on cartesius... not there yet"
    This reverts commit ada06061be31c2857e50bc2f197209e55ef82274.

--- notes/i-emic_cmake_examples/cartesius.cmake
+++ notes/i-emic_cmake_examples/cartesius.cmake
@@ -1,8 +1,3 @@
 rm -rf CMakeCache.txt CMakeFiles
 
-cmake \
--DMRILU_DIR=/home/emulder/mrilu/ifort \
--DTrilinos_DIR=/home/emulder/trilinos/11.14 \
--DGTEST_DIR=/hpc/sw/gtest-1.7.0/ \
-\
-../
+cmake ../	
