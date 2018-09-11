950ce913bdcf5565fa12fb8b8f29d25b3e1f667e
erik@laptop <t.e.mulder@uu.nl>
2018-09-11 13:44:00 +0200

    * src/main/run_ocean.C: fixed timer bug * src/ocean/probe.F90: apply sea ice mask to fluxes first * test/tuning/coupledmodel_params.xml: time dependent parameters

--- src/main/run_ocean.C
+++ src/main/run_ocean.C
@@ -86,14 +86,12 @@ void runOceanModel(RCP<Epetra_Comm> Comm)
     // Run continuation
 	continuation.run();
     
+	TIMER_STOP("Total time...");
+
     // print the profile
     if (Comm->MyPID() == 0)
     {
         printProfile(profile);
         jdqz->printProfile("jdqz_profile");
     }
-
-	//------------------------------------------------------------------
-	TIMER_STOP("Total time...");		
 }
-
