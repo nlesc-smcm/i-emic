cdefd046a8715a4592f5cfaf7e80263ae8453419
erik@laptop <t.e.mulder@uu.nl>
2018-10-13 20:55:59 +0200

    improved plotting

--- matlab/exportfig.m
+++ matlab/exportfig.m
@@ -11,39 +11,21 @@ function [] = exportfig(string,fs,dims,invert)
 
     width = dims(1); height = dims(2);
     set(gca,'fontsize',fs)
+    set(gcf,'color','w')
 
     set(gcf,'paperunits','centimeters')
     set(gcf,'paperposition',[1,1,width,height])
     set(gcf,'papersize',[width,height])
 
     if invert
-        set(gca,'color','k');
-        set(gca,'xcolor','w');
-        set(gca,'ycolor','w');
-        set(gca,'gridcolormode','manual');
-        set(gca,'minorgridcolormode','manual');
-        set(gca,'gridcolor',[.15,.15,.15]);
-        set(gca,'minorgridcolor',[.15,.15,.15]);
-        set(gca,'gridalpha',1);
-        set(gca,'minorgridalpha',1);
-        
-        set(gcf,'color','k');
-        set(legend,'color','k');
-        set(legend,'edgecolor',[.5,.5,.5]);
-        set(legend,'textcolor','w');        
-
-        title = get(gca,'title');
-        title.Color=[1,1,1];
-        
-        %set(colorbar, 'color', 'w');
-        
+        invertcolors();        
     end
     
     set(gcf,'inverthardcopy','off')
     fprintf('saving to %s...\n', string);
     %print(string, '-depsc2', '-painters', '-opengl');
-    print(string, '-depsc2', '-painters');
-    %print(string, '-depsc2');
+    %print(string, '-depsc2', '-painters');
+    print(string, '-depsc2');
     %print(string)
     %saveas(gcf,string);
 end
--- matlab/plot_cdata.m
+++ matlab/plot_cdata.m
@@ -1,31 +1,77 @@
-function [titles, cdata] = plot_cdata(fname, lsty)
+function [titles, cdata] = plot_cdata(fname, opts)
+    plot_fancy=false;
+    npfiles=0
+    lsty = 'k.-';
     
-    if nargin < 2
-        lsty = 'k.-';
-    end
+    if nargin > 1
+        if isfield(opts,'colors')
+            plot_fancy=true;
+            coldata = opts.colors;
+        end
+        
+        if isfield(opts,'lsty')
+            lsty = opts.lsty;
+        end
+
+        if isfield(opts,'prepend') && isfield(opts,'prefiles')
+            npfiles  = opts.prepend;
+            prefiles = opts.prefiles;
+        end
+    end        
+            
     if (nargin < 1)
         fname = 'cdata.txt'
     end
-    
-    [titles, cdata] = load_cdata(fname);
+
+    cdata = [];
+    for i = 1:npfiles
+        [t,c] = load_cdata(prefiles{i});
+        cdata = [cdata; c];
+    end       
+        
+    [titles, c] = load_cdata(fname);
+    cdata = [cdata; c];
 
     maxPsi = 0;
     minPsi = 0;
     
+    if plot_fancy
+        for i = 1:size(cdata,2)
+            if strcmp(titles{i}, coldata)
+                cmax = max(cdata(:,i));
+                cmn  = mean(cdata(:,i));
+                cmap = jet(cmax);
+                %cmap = my_colmap([0,cmax], -1, cmax);
+                clrs = cmap(cdata(:,i),:);
+                break;            
+            end
+        end
+    end
+    
+    
+    
     for i = 2:size(cdata,2)
         figure(i)
+
         if strcmp(titles{i}, 'max(Psi)')
             maxPsi = i;
         elseif strcmp(titles{i}, 'min(Psi)')
             minPsi = i;
         end
         
-        if strcmp(titles{i}, '||F||')
+        if strcmp(titles{i}, '||F||') || strcmp(titles{i}, 'ds') || ...
+                strcmp(titles{i}, 'Tol')
+            cdata(:,i) = abs(cdata(:,i));
             semilogy(cdata(:,1),cdata(:,i),lsty);
             xlabel('par');
         else
             plot(cdata(:,1),cdata(:,i),lsty);
             xlabel('par');
+        end
+        if plot_fancy
+            hold on
+            s = scatter(cdata(:,1),cdata(:,i), 20, clrs, 'o','filled'); hold off
+            uistack(s,'down');
         end
         title(titles{i})
         grid on;
