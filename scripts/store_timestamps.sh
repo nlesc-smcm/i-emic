414aa4e3e3a6e2d95db76554adca318448a6d3b7
Sven Baars <s.baars@rug.nl>
2019-02-19 21:21:28 +0100

    Fix restoring timestamps if no diff can be made

--- scripts/store_timestamps.sh
+++ scripts/store_timestamps.sh
@@ -3,4 +3,6 @@
 find . -type f -printf 'touch --no-create -d "%t" "%p"\n' > ${IEMIC_BUILD_DIR}/restore_timestamps.sh
 rev=`git rev-parse HEAD`
 echo "changed_files=\`git diff --name-only $rev HEAD\`
-touch --no-create \`echo \$changed_files\`" >> ${IEMIC_BUILD_DIR}/restore_timestamps.sh
+if [ -n \"$changed_files\" ]; then
+    touch --no-create \`echo \$changed_files\`;
+fi" >> ${IEMIC_BUILD_DIR}/restore_timestamps.sh
