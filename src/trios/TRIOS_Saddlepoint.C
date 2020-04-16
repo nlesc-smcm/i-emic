b3553c23178344dfe536709c17c2845161d9d070
Sven Baars <s.baars@rug.nl>
2020-04-16 13:41:32 +0200

    Remove an unused include from GlobalDefinitions

--- src/trios/TRIOS_Saddlepoint.C
+++ src/trios/TRIOS_Saddlepoint.C
@@ -11,15 +11,19 @@
 #include "Epetra_CrsMatrix.h"
 #include "Epetra_Import.h"
 #include "Epetra_Comm.h"
+
 #include "AztecOO.h"
+
 #include "EpetraExt_MatrixMatrix.h"
-#include "Teuchos_ParameterList.hpp"
+#include "EpetraExt_VectorOut.h"
+
 #include "Utils.H"
 
 #include "TRIOS_Macros.H"
 #include "TRIOS_Saddlepoint.H"
 #include "TRIOS_SolverFactory.H"
 
+#include "Teuchos_ParameterList.hpp"
 #include "Teuchos_FancyOStream.hpp"
 
 #ifdef HAVE_MPI
